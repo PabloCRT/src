863fa0e9a5908bc12f313fc0d30f1feed29a7872
Pablo Silva <pablooom.silva@gmail.com>
2024-11-17 18:42:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function voltar() {
     imagem1.onload = function() {
         imagem1.src = 'assets/palco.jpg';
     };
-
+//-----------------------------------------------------------
     // Forçar o navegador a recarregar a imagem
     imagem1.src = '';
     imagem1.src = 'assets/palco.jpg';
@@ -12,12 +12,18 @@ function voltar() {
     document.getElementById('bola1').style.backgroundColor = "#8929a3b4"
     document.getElementById('bola2').style.backgroundColor = "transparent"
 
+//-----------------------------------------------------------
+
     //transições entre imagem e legenda
 
     document.getElementById('bola1').style.transition = "1s"
     document.getElementById('bola2').style.transition = "1s"
 
     document.getElementById('img1').style.transition = "1s"
+
+    //troca de legenda
+    const legenda = document.getElementById("legenda-foto");
+    legenda.textContent = "Premiação 3º geral (Regional)"
 
 //-----------------------------------------------------------
 
@@ -37,6 +43,17 @@ function avancar() {
 
     document.getElementById('bola2').style.backgroundColor = "#8929a3b4"
     document.getElementById('bola1').style.backgroundColor = "transparent"
+
+    //transição entra imagem e legenda
+
+    document.getElementById('bola1').style.transition = "1s"
+    document.getElementById('bola2').style.transition = "1s"
+
+    document.getElementById('img1').style.transition = "1s"
+
+    //troca de legenda
+    const legenda = document.getElementById("legenda-foto");
+    legenda.textContent = "Foto em conjunto da equipe (Regional)"
 
 //-----------------------------------------------------------
 }
