31e7ca1eaedc77945e1460843ec3b404fc1898ab
Pablo Silva <pablooom.silva@gmail.com>
2024-12-01 17:15:25 -0300

    Update nav.js

--- pages/nav.js
+++ pages/nav.js
@@ -1,3 +1,8 @@
 function scrollToHeader(){
     document.getElementById('corpo').style.scrollBehavior="smooth"
+
+    const meuCheckBox = document.querySelectorAll('input[type="checkbox"]');
+    meuCheckBox.forEach(checkbox => {
+        checkbox.checked = false;
+    });
 }
