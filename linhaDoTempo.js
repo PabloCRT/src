9a9550948c7062f5958c16f89f7fca61f0064fa4
Pablo Silva <pablooom.silva@gmail.com>
2024-12-06 11:22:36 -0300

    Update linhaDoTempo.js

--- linhaDoTempo.js
+++ linhaDoTempo.js
@@ -7,9 +7,7 @@ function trocar1(){
 
     const imagem = document.getElementById('imagem');
 
-    imagem1.onload = function() {
         imagem.src = 'assets/premio.jpg';
-    };
 
 }
 
@@ -24,8 +22,36 @@ function trocar2(){
 
     const imagem = document.getElementById('imagem');
 
-    imagem1.onload = function() {
-        imagem.src = 'assets/premio.jpg';
-    };
+        imagem.src = 'assets/interno.jpg';
+
+}
+
+
+
+function trocar3(){
+
+    document.getElementById("primeiro").style.backgroundColor="white"
+    document.getElementById("segundo").style.backgroundColor="white"
+    document.getElementById("terceiro").style.backgroundColor="#8929a3b4"
+    document.getElementById("quarto").style.backgroundColor="white"
+
+    const imagem = document.getElementById('imagem');
+
+        imagem.src = 'assets/palco.JPG';
+
+}
+
+
+
+function trocar4(){
+
+    document.getElementById("primeiro").style.backgroundColor="white"
+    document.getElementById("segundo").style.backgroundColor="white"
+    document.getElementById("terceiro").style.backgroundColor="white"
+    document.getElementById("quarto").style.backgroundColor="#8929a3b4"
+
+    const imagem = document.getElementById('imagem');
+
+        imagem.src = 'assets/3lugarGeral.jpg';
 
 }
