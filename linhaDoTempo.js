4394453d620150f707cc1b435b4a292c9b0f8fa1
Pablo Silva <pablooom.silva@gmail.com>
2024-12-06 15:25:05 -0300

    Update linhaDoTempo.js

--- linhaDoTempo.js
+++ linhaDoTempo.js
@@ -4,7 +4,8 @@ function trocar1(){
     document.getElementById("segundo").style.backgroundColor="white"
     document.getElementById("terceiro").style.backgroundColor="white"
     document.getElementById("quarto").style.backgroundColor="white"
-    document.getElementById("texto").textContent="Melhor Organização & Método"
+    document.getElementById("texto").textContent="Melhor Organização & Método (Interno)."
+    document.getElementById("primeiro").style.color="#f5d276"
 
     const imagem = document.getElementById('imagem');
 
@@ -20,7 +21,8 @@ function trocar2(){
     document.getElementById("segundo").style.backgroundColor="#8929a3b4"
     document.getElementById("terceiro").style.backgroundColor="white"
     document.getElementById("quarto").style.backgroundColor="white"
-    document.getElementById("texto").textContent="Classificação regional"
+    document.getElementById("texto").textContent="Classificação regional (Interno)."
+    document.getElementById("segundo").style.color="#f5d276"
 
     const imagem = document.getElementById('imagem');
 
@@ -36,7 +38,8 @@ function trocar3(){
     document.getElementById("segundo").style.backgroundColor="white"
     document.getElementById("terceiro").style.backgroundColor="#8929a3b4"
     document.getElementById("quarto").style.backgroundColor="white"
-    document.getElementById("texto").textContent="3° lugar geral"
+    document.getElementById("texto").textContent="3° lugar geral (Regional)."
+    document.getElementById("terceiro").style.color="#f5d276"
 
     const imagem = document.getElementById('imagem');
 
@@ -52,7 +55,8 @@ function trocar4(){
     document.getElementById("segundo").style.backgroundColor="white"
     document.getElementById("terceiro").style.backgroundColor="white"
     document.getElementById("quarto").style.backgroundColor="#8929a3b4"
-    document.getElementById("texto").textContent="Classificação para o nacional"
+    document.getElementById("texto").textContent="Classificação para o nacional (Regional)."
+    document.getElementById("quarto").style.color="#f5d276"
 
     const imagem = document.getElementById('imagem');
 
